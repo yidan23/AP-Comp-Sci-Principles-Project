ff45ce10c11fe86df5137b7a6af44a577bba1ae9
yidan23 <77285801+yidan23@users.noreply.github.com>
2021-03-30 11:10:07 -0400

    Add files via upload
    Just a simple blinking light test to get familiar with Arduino IDE and basic hardware

---
+++ Daniel_s_Blinds_Project.ino
@@ -0,0 +1,15 @@
+
+const int led = 9;
+
+void setup() {
+  
+pinMode (led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite (led, HIGH);
+  delay(1000);
+  digitalWrite (led, LOW);
+  delay (1000);
+
+}
